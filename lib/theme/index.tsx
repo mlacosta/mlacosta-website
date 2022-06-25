7311c5a41aea9941708f28509ffd4c794a56de7a
Mariano L. Acosta <mariano@calyptia.com>
2022-06-25 17:57:54 -0300

    set MUI

---
+++ lib/theme/index.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "hsl(219, 37%, 17%)",
+      dark: "hsl(211, 53%, 11%)",
+      light: "hsl(212, 29%, 36%)",
+    },
+    text: {
+      primary: "hsl(75, 6%, 87%)",
+      secondary: "hsl(214, 23%, 56%)",
+    },
+    background: {
+      default: "hsl(219, 37%, 17%)",
+      paper: "hsl(0, 0%, 100%)",
+    },
+  },
+});
